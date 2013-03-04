c6ec9457a250e7508ca125470d3e7c02131353b9
aagohary <aagohary@gmail.com>
2013-03-04 16:55:32 -0500

    greedy nystrom

--- R/GreedyNystrom.r
+++ R/GreedyNystrom.r
@@ -15,13 +15,50 @@ GreedyNystrom <- function(X,m, kernelFunc,k,c){
   f <- apply(G,2,function(x){sum(x^2)})
   g <- diag(K)
   selectedCols <- c()
-  
+  W <- matrix(nrow=n,ncol=0)
+  V <- matrix(nrow=c,ncol=0)
   #selecting columns
   for(t in 1:m){
-    q <- which.max(f/g)
+    scores <- f/g
+    scores[selectedCols] <- scores[is.nan(scores)] <- scores[is.infinite(scores)] <- 0
+    q <- which.max(f/g)    
+    
+    if(scores[q]==0){
+      print("max score is zero")
+      break;  
+    }    
+    
+    delta <- ifelse(t==1, K[,q], K[,q] - W %*% W[q,])
+    gam <- ifelse(t==1, G[,q], G[,q] - V %*% W[q,])        
+    alphaSqrt = sqrt(delta[q])
+    w <- delta/alphaSqrt
+    v <- gam/alphaSqrt
+    #update f and g
+    r2 <- ifelse(t==1,0, W %*% (v%*%V))
+    
+    if(sum(delta^2)<1e-10 || alphaSqrt<1e-10){
+      print("t > rank(K)")
+      break
+    }
+  
+    r1 <- V%*%v
+    r3 <- w^2
+    
+    f <- f - 2*(w * (r1 - r2)) + (sum(v^2))*r3
+    g <- g - r3
+    
+    f[q] <- g[q] <- f[f<1e-10] <- g[g<1e-10] <- 0    
+    
+    
+    #updates for the next iterations
+    W <- cbind(W,w)
+    V <- cbind(V,v)    
     selectedCols <- c(selectedCols, q)
-    delta <- K[,q] - 
   }
+  #compute the embedding
+  e <- eigenn(W%*%t(W))
+  Y <- t(e$vectors[,1:k])%*%W
+  return(Y)
 }
 
 #computes the full kernel matrix
